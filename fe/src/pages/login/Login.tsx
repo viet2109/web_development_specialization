3e39338899311be540f9705d56ba014a297c5496
Mai Xuân Thức <xuanthuc254@gmail.com>
2025-03-31 20:50:12 +0700

    connect api login, sign up fe

--- fe/src/pages/login/Login.tsx
+++ fe/src/pages/login/Login.tsx
@@ -2,11 +2,37 @@ import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import "./login.css";
+import { useDispatch } from "react-redux";
+import { login } from "../../api/api";
+import { loginSuccess } from "../../redux/authSlice";
+import ClipLoader from "react-spinners/ClipLoader";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  
 
   
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); 
+    setLoading(true);
+    try {
+      const data = await login(email, password);
+      
+      dispatch(loginSuccess({ token: data.accessToken, user: data.user }));
+      
+      navigate("/"); 
+    } catch (error) {
+      console.error("Login failed:", error);
+      toast.error('❌ Invalid your email or password. Please check your-email', {
+              position: 'top-center',
+            })
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="login">
@@ -25,23 +51,32 @@ const Login = () => {
         </div>
         <div className="right">
           <h1>Login</h1>
-          <form >
+          <form  onSubmit={handleLogin}>
             <input
               type="text"
-              placeholder="Username"
-              name="username"
-          
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
             <input
               type="password"
               placeholder="Password"
               name="password"
-            
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               required
             />
-          
-            <button type="submit">Login</button>
+           <button type="submit" disabled={loading}>
+        {loading ? (
+          <>
+            <ClipLoader size={16} color="#000" /> 
+          </>
+        ) : (
+          'Login'
+        )}
+      </button>
           </form>
         </div>
       </div>
@@ -50,3 +85,5 @@ const Login = () => {
 };
 
 export default Login;
+
+
--- fe/src/pages/register/Register.tsx
+++ fe/src/pages/register/Register.tsx
@@ -2,10 +2,49 @@
 import { Link, useNavigate } from "react-router-dom";
 
 import "../register/Register.css"
+import { useState } from "react";
+import { register } from "../../api/api";
+import { ToastContainer, toast } from "react-toastify";
+import { ClipLoader } from 'react-spinners';
 
+type Gender = "MALE" | "FEMALE" | "OTHER";
 const Register = () => {
-  
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [firstName, setfirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [gender, setGender] = useState<Gender>("MALE");
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
 
+    try {
+      await register(email, password, firstName, lastName, gender);
+      toast.success(
+        `🎉 Registration successful! Please check your email (${email}) to verify your account.`,
+        {
+          position: 'top-center',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          theme: 'colored',
+        }
+      );
+      navigate("/login");
+    } catch (error) {
+      console.error('Registration failed:', error);
+      toast.error('❌ Registration failed. Please try again.', {
+        position: 'top-center',
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="register">
       <div className="card">
@@ -23,33 +62,61 @@ const Register = () => {
         </div>
         <div className="right">
           <h1>Register</h1>
-          <form>
-            <input
-              type="text"
-              placeholder="Username"
-              name="username"
-              
-            />
+          <form onSubmit={handleRegister}>
+           
             <input
               type="email"
               placeholder="Email"
-              name="email"
-              
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
             <input
               type="password"
               placeholder="Password"
               name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
            
             />
             <input
               type="text"
-              placeholder="Name"
-              name="name"
+              placeholder="firstName"
+              value={firstName}
+              name="firstName"
+              onChange={(e) => setfirstName(e.target.value)}
+              required
               
             />
-           
-            <button >Register</button>
+             <input
+              type="text"
+              placeholder="lastName"
+              value={lastName}
+              name="lastName"
+              onChange={(e) => setLastName(e.target.value)}
+              required
+              
+            />
+            <select
+              value={gender}
+              onChange={(e) => setGender(e.target.value as Gender)}
+              required
+            >
+              <option value="MALE">Male</option>
+              <option value="FEMALE">Female</option>
+              <option value="OTHER">Other</option>
+            </select>
+            
+            <button type="submit" disabled={loading}>
+        {loading ? (
+          <>
+            <ClipLoader size={16} color="#000" /> 
+          </>
+        ) : (
+          'Register'
+        )}
+      </button>
           </form>
         </div>
       </div>
